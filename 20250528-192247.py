0d71ea8afd1a706602da6ff755c32f4e9e3d22f7
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-05-28 19:56:34 +0500

    Chapter4

---
+++ 20250528-192247.py
@@ -0,0 +1,12 @@
+ #List of pizza names
+pizzas = ["pepperoni", "margherita", "bbq chicken", "hawaiian", "veggie"]
+
+ #For loop to print a sentence about each pizza
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+
+
+print("\nPizza is one of my favorite foods!")
+print("I enjoy trying new toppings and flavors.")
+print("Whether it's thick crust or thin crust, I always enjoy a good slice.")
+print("I really love pizza!")
