450b5bbfac5b4abf2203ab1fa724fee65cd0b9ef
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-05-31 14:29:34 +0500

    Update 20250531-142613.py

--- 20250531-142613.py
+++ 20250531-142613.py
@@ -1,3 +1,13 @@
-# Using range with a step of 2 to get odd numbers
+Question 6______
+Use the third argument of the 
+range() function to make a
+list of the odd numbers from 
+1 to 20. Use a for loop to print
+each number 
+
+solution____
+
+ Using range with a step of 
+2 to get odd numbers
 for number in range(1, 21, 2):
     print(number12)
