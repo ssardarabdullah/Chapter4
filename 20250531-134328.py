8538fa06dbf24f7e018b91f0f42cdc275aa80b08
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-05-31 13:56:07 +0500

    Update 20250531-134328.py

--- 20250531-134328.py
+++ 20250531-134328.py
@@ -1,25 +1,42 @@
-# Original menu as a tuple (immutable)
+Question 2 ________
+Use a for loop to print each food the restaurant offers.
+
+•	 Try to modify one of the items, and make sure that Python rejects the 
+
+change.
+
+•	 The restaurant changes its menu, replacing two of the items with different 
+
+foods. Add a line that rewrites the tuple, and then use a for loop to print 
+
+each of the items on the revised menu.
+
+
+solution _____
+
+
+Original menu as a tuple (immutable)
 menu = ("Pizza", "Burger", "Pasta", "Salad", "Soup")
 
-# Print each food item
-print("Original Menu:")
+  Print each food item
+  print("Original Menu:")
 for item in menu:
     print(item)
-    # Create a new tuple with some items changed
+     Create a new tuple with some items changed
 menu = ("Sushi", "Burger", "Tacos", "Salad", "Ramen")
 
-# Print revised menu
+ Print revised menu
 print("\nRevised Menu:")
 for item in menu:
     print(item)
-    #Original Menu:
+    output_______
 Pizza
 Burger
 Pasta
 Salad
 Soup
 
-#Revised Menu:
+Revised Menu:
 Sushi
 Burger
 Tacos
