deadea413d3e1afe4ba325b4a71acc009f1290da
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-05-31 14:15:33 +0500

    Update 20250531-140834.py

--- 20250531-140834.py
+++ 20250531-140834.py
@@ -1,18 +1,30 @@
+Question 4
+Think of at least three differnce .
+Store the names of these animals in a list
+, and then use a for loop to print out the
+name of each animal. •	
+Modify your program to print a statement
+about each animal, such as A dog would
+make a great pet. •
+Add a line at the end of your program stating what these animals have in common. 
+You could print a sentence such as Any of
+these animals would make a great pet!
 
+solution _______
 
 animals = ['dog', 'cat', 'rabbit']
 
- # Print each animal's name
+   Print each animal's name
 for animal in animals:
     print(animal)
 
-print()  # Blank line for better readability
+print()  Blank line for better readability
 
-# Print a statement about each animal
+ Print a statement about each animal
 for animal in animals:
     print(f"A {animal} would make a great pet.")
 
-# Final statement about what they have in common
+ Final statement about what they have in common
 print("\nAny of these animals would make a great pet!")
 dog
 cat
